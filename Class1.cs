281acdff1a880afe72f260f48cfc5893f077b7c0
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-29 12:08:55 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class Student
+{
+	public int Mid;
+	public int Final;
+	public Student(int m,int f)
+	{
+		Mid = m;
+		Final = f;
+	}
+	public static Student operator+(Student s1,Student s2)
+	{
+		Student temp = new Student(0, 0);
+		temp.Mid = (s1.Mid + s2.Mid) / 2;
+		temp,Final = (s1.Final + s2.Final) / 2;
+		return temp;
+		
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class_05_29_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(86, 96);
+            Student s2 = new Student(60, 75);
+
+            Student savg = s1 + s2;
+            Console.WriteLine(savg.Mid);
+            Console.WriteLine(savg.Final);
+
+        }
+    }
+}
